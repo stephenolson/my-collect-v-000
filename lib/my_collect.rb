f143177923ecce206df80a2aa0aa94ef4775a6e1
Stephen Olson <stephenolson@gmail.com>
2018-09-27 18:08:06 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
-def my_collect(array)
-  x = 0
-  collection = []
-  while x < array.length
-    collection << yield(array[x])
-    x += 1
+def my_collect(array) #argument of an array
+  i = 0 # sets counter to 0
+  collection = [] # creates a new array
+  while i < array.length # index is less than the total items in array
+    collection << yield(array[i]) # move original array item to new array
+    i += 1 # move up counter / array index
   end
   collection
 end
